65a9ebdf0edd7ae4093a4c24cafe5842b2115842
Cherkashin Alexander <cherkalexander@gmail.com>
2019-02-17 21:58:24 +0300

    Display maps

--- client/src/Components/TourDesigner/TourDesigner.jsx
+++ client/src/Components/TourDesigner/TourDesigner.jsx
@@ -9,7 +9,8 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
-import { Map, TileLayer, Popup, Circle } from 'react-leaflet';
+import { Map, TileLayer, Popup, Circle, ImageOverlay } from 'react-leaflet';
+import L from 'leaflet';
 import EditTourPanel from './EditTourPanel';
 import { observer, inject } from 'mobx-react';
 
@@ -44,12 +45,12 @@ const TourDesigner = inject("tourStore")(observer(class TourDesigner extends Rea
 
         this._handleSave = this._handleSave.bind(this);
         this._handleClose = this._handleClose.bind(this);
+        this._handleMapClick = this._handleMapClick.bind(this);
         this._handleNameChanged = this._handleNameChanged.bind(this);
     }
 
     state = {
-        lat: 51.505,
-        lng: -0.09,
+        position: [0, 0],
         zoom: 13,
     };
 
@@ -66,13 +67,53 @@ const TourDesigner = inject("tourStore")(observer(class TourDesigner extends Rea
         this.editingTour.name = e.name;
     }
 
+    _handleMapClick(e) {
+        console.log(e);
+    }
+
     get editingTour() {
         return this.props.tourStore.editingTour;
     }
 
+    _renderImageMap() {
+        debugger;
+        const { classes } = this.props;
+        const bounds = [[0, 0], [1000, 1000]];
+
+        return (
+            <Map сrs={L.CRS.Simple} bounds={bounds}
+                className={classes.map}
+            >
+                <ImageOverlay
+                    url='https://st3.depositphotos.com/1000434/13699/v/1600/depositphotos_136991736-stock-illustration-abstract-vector-plan-of-office.jpg'
+                    bounds={bounds}
+                />
+            </Map>
+        );
+    }
+
+    _renderEarthMap() {
+        const { classes } = this.props;
+
+        return (
+            <Map
+                center={this.state.position}
+                zoom={this.state.zoom}
+                className={classes.map}
+                onclick={this._handleMapClick}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {/* <Circle center={position} radius={30} fill={true}>
+                    <Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup>
+                </Circle> */}
+            </Map>
+        );
+    }
+
     render() {
         const { classes } = this.props;
-        const position = [this.state.lat, this.state.lng];
 
         return (
             <Dialog
@@ -90,15 +131,8 @@ const TourDesigner = inject("tourStore")(observer(class TourDesigner extends Rea
                     </Toolbar>
                 </AppBar>
                 <div className={classes.content}>
-                    <Map center={position} zoom={this.state.zoom} className={classes.map}>
-                        <TileLayer
-                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Circle center={position} radius={30} fill={true}>
-                            <Popup>A pretty CSS3 popup. <br /> Easily customizable.</Popup>
-                        </Circle>
-                    </Map>
+                    {this.editingTour.mapType === 'Earth' && this._renderEarthMap()}
+                    {this.editingTour.mapType === 'Image' && this._renderImageMap()}
                     <EditTourPanel tour={this.editingTour} onNameChanged={this._handleNameChanged} />
                 </div>
 
