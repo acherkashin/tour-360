bee03fa2b2d85c7cb8f22f7d2d310c9cc9fe5bf3
Cherkashin Alexander <cherkalexander@gmail.com>
2019-03-17 22:16:41 +0300

    feat(frontend): user sliders instead of inputs to set angle
    y
    
    МЕТА ДАННЫЕ: y

--- client/src/Components/TourDesigner/EditConnectionPanel.jsx
+++ client/src/Components/TourDesigner/EditConnectionPanel.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import { withStyles } from '@material-ui/core/styles';
-import { TextField } from '@material-ui/core';
+import { TextField, Typography } from '@material-ui/core';
+import Slider from '@material-ui/lab/Slider';
 
 const styles = theme => ({
     root: {
         flex: 1,
         padding: theme.spacing.unit * 2,
+        // slider causes overflow of panel
+        overflow: 'hidden',
     },
+    slider: {
+        padding: '16px 12px',
+    },
+    thumb: {
+        width: 20,
+        height: 20,
+    }
 });
 
 const EditConnectionPanel = observer(class EditConnectionPanel extends React.Component {
@@ -19,50 +29,55 @@ const EditConnectionPanel = observer(class EditConnectionPanel extends React.Com
         this._handleEndPlacePositionChanged = this._handleEndPlacePositionChanged.bind(this);
     }
 
-    _handleStartPlacePositionChanged(e) {
+    _handleStartPlacePositionChanged(e, value) {
         this.props.onStartPlacePositionChanged({
             origin: this,
-            value: parseInt(e.target.value) || 0,
+            value,
         })
     }
 
-    _handleEndPlacePositionChanged(e) {
+    _handleEndPlacePositionChanged(e, value) {
         this.props.onEndPlacePositionChanged({
             origin: this,
-            value: parseInt(e.target.value) || 0,
+            value,
         });
+    }
+
+    _renderPlacePosition(options) {
+        const { classes } = this.props;
+        const { id, label, value, onChange } = options;
+
+        return <>
+            <Typography id={id}>{label}</Typography>
+            <Slider
+                classes={{ container: classes.slider, thumb: classes.thumb }}
+                value={value}
+                aria-labelledby={id}
+                min={0}
+                max={359}
+                step={1}
+                onChange={onChange}
+            />
+            <Typography variant="caption" align="right">{value}</Typography>
+        </>;
     }
 
     render() {
         const { classes, connection } = this.props;
 
         return <div className={classes.root}>
-            <TextField
-                label="Start Place Position"
-                value={connection.startPlacePosition}
-                onChange={this._handleStartPlacePositionChanged}
-                margin="normal"
-                fullWidth={true}
-                type={'number'}
-                inputProps={{
-                    min: 0,
-                    max: 359
-                }}
-                autoFocus
-            />
-            <TextField
-                label="End Place Position"
-                value={connection.endPlacePosition}
-                onChange={this._handleEndPlacePositionChanged}
-                margin="normal"
-                fullWidth={true}
-                type={'number'}
-                inputProps={{
-                    min: 0,
-                    max: 359
-                }}
-            />
-
+            {this._renderPlacePosition({
+                id: "start-place-position",
+                label: "Start Place Position",
+                value: connection.startPlacePosition,
+                onChange: this._handleStartPlacePositionChanged,
+            })}
+            {this._renderPlacePosition({
+                id: "end-place-position",
+                label: "End Place Position",
+                value: connection.endPlacePosition,
+                onChange: this._handleEndPlacePositionChanged,
+            })}
         </div>;
     }
 });
