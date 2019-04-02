6499c98042c1bc654ac03e30162553990b4d454b
Cherkashin Alexander <cherkalexander@gmail.com>
2019-04-02 22:31:38 +0300

    refactor(frontend): add index to TourDesiger folder

---
+++ client/src/Components/TourDesigner/index.js
@@ -0,0 +1,25 @@
+import Connection from './Connection';
+import ConnectionItem from './ConnectionItem';
+import ConnectionList from './ConnectionList';
+import EditConnectionPanel from './EditConnectionPanel';
+import EditPlacePanel from './EditPlacePanel';
+import EditTourPanel from './EditTourPanel';
+import MapEditMode from './MapEditMode';
+import Place from './Place';
+import PlacePosition from './PlacePosition';
+import SoundEditor from './SoundEditor';
+import TourDesigner from './TourDesigner';
+
+export {
+    Connection,
+    ConnectionItem,
+    ConnectionList,
+    EditConnectionPanel,
+    EditPlacePanel,
+    EditTourPanel,
+    MapEditMode,
+    Place,
+    PlacePosition,
+    SoundEditor,
+    TourDesigner,
+};
