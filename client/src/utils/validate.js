8cbe53cebeb2c44b1461993d0a03d55aede99598
Cherkashin Alexander <cherkalexander@gmail.com>
2019-04-06 16:18:41 +0300

    fix(frontend): fix validation confirm password error

--- client/src/utils/validate.js
+++ client/src/utils/validate.js
@@ -1,7 +1,7 @@
-export const validEmail = (value) => {
+export function validEmail(value) {
     value = value.trim();
     const emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
+
     if (value === '') {
         return { valid: false, error: 'please fill out this field' };
     }
@@ -9,7 +9,7 @@ export const validEmail = (value) => {
     return emailRe.test(value) ? { valid: true, error: '' } : { valid: false, error: 'wrong email' };
 };
 
-export const validPassword = (value) => {
+export function validPassword(value) {
     const passwordRe = /^(.{8,})$/;
 
     if (value.trim() === '') {
@@ -19,25 +19,25 @@ export const validPassword = (value) => {
     return passwordRe.test(value) ? { valid: true, error: '' } : { valid: false, error: 'password must be at least 8 characters long' };
 };
 
-export const validName = (value) => {
+export function validName(value) {
     value = value.trim();
     const nameRe = /^([a-zA-Z]|[а-яА-Я]){2,}$/;
 
     if (value === '') {
         return { valid: false, error: 'please fill out this field' };
     }
-    
+
     if (value.length < 2) {
         return { valid: false, error: 'name must be at least 2 characters long' };
     }
-    
+
     return nameRe.test(value) ? { valid: true, error: '' } : { valid: false, error: 'name must contain only latin and russian characters' };
 };
 
-export const validConfirmationPassword = (value) => {
+export function validConfirmationPassword(value) {
     const { confirmationPassword, password } = value;
 
-    if (value.trim() === '') {
+    if (confirmationPassword.trim() === '') {
         return { valid: false, error: 'please fill out this field' };
     }
 
