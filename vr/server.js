fe58f2a86d61aec4498e79a0993238deb9c1fd39
Cherkashin Alexander <cherkalexander@gmail.com>
2019-06-13 01:09:04 +0300

    try to fix heroku build

--- vr/server.js
+++ vr/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8081;
 const app = express();
+const port = process.env.PORT || 8081;
 // app.use(favicon(__dirname + '/build/favicon.png'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
